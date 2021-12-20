c059d3345a0cfaec976542849fce012093469037
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-20 16:44:00 +0530

    add the file

---
+++ Example.java
@@ -0,0 +1,13 @@
+class Example{
+	public static void main(String []args){
+		System.out.println(xyz[0]);
+		System.out.println(xyz[3]);
+		System.out.println(xyz[4]);
+	}
+		
+	
+	static String []xyz = {"mahesh","umesh","raju","xyz"};
+}
+
+	
+	
